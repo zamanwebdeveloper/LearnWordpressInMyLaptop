b2a4cc9b58b54fc1469fa293a859bcdd2a342992
Syed Zaman Mostafiz <zamanwebdeveloper>
2019-09-07 23:35:08 +0600

    5/58 WordPress Theme Development part-5. How to Create Post Thumbnails Featured Images in WordPress Learning Completed.

---
+++ zamanwebdeveloper/functions.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* Registering Menu Support
+*/
+
+function myWebsiteMenu(){
+	if(function_exists('register_nav_menu')){
+		register_nav_menu( 'header_top_menu', __( 'Header Menu', 'ZamanWebDeveloper' ) );
+		register_nav_menu( 'footer_menu', __( 'Footer Menu', 'ZamanWebDeveloper' ) );
+	}
+}
+add_action('init','myWebsiteMenu');
+
+/**
+ * Featured Image Support
+ */
+add_theme_support( 'post-thumbnails', array( 'post', 'page') );
+set_post_thumbnail_size( 200, 200, true );
+
+add_image_size( 'myFituredImage', 300, 300, true );
+add_image_size( 'onlywidthimage', 300, true );
