41fe457deb4c1c763fe10801f5e7c502c5e08427
Syed Zaman Mostafiz <zamanwebdeveloper>
2019-09-16 00:47:47 +0600

    17/58 Wordpress Theme Development part-16 .How to create Custom and Specialized Page Templates part-1 Learning Completed.

--- themes/zamanwebdeveloper/page.php
+++ themes/zamanwebdeveloper/page.php
@@ -9,16 +9,8 @@
             <div class="row">
                 <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12 ftl">
                     <div class="single-page-details">
-                        <h2><?php the_title(); ?></h2>
+                        <h2><?php the_title(); ?> This is page.php</h2>
                         <?php the_content(); ?>                       
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="comment-area">
-                                <h2 class="page-header">Comments</h2>
-                                <?php comments_template( '', true ); ?>                               
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <?php get_template_part("sidebar"); ?>
--- themes/zamanwebdeveloper/template-fluid.php
+++ themes/zamanwebdeveloper/template-fluid.php
@@ -1,4 +1,8 @@
-<?php get_header(); ?>
+<?php 
+/*
+    Template Name: Full Width
+*/
+get_header(); ?>
     <!-- Header area end -->
     <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
@@ -7,21 +11,12 @@
     <div id="news-area" class="section-gray news-section-single">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12 ftl">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ftl">
                     <div class="single-page-details">
-                        <h2><?php the_title(); ?></h2>
+                        <h2><?php the_title(); ?> This is template-fluid.php</h2>
                         <?php the_content(); ?>                       
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="comment-area">
-                                <h2 class="page-header">Comments</h2>
-                                <?php comments_template( '', true ); ?>                               
-                            </div>
-                        </div>
-                    </div>
                 </div>
-                <?php get_template_part("sidebar"); ?>
             </div>
         </div>
     </div>
