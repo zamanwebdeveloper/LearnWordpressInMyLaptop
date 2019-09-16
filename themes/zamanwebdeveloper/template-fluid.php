9ca0c3c086e08543c7b3305e83bfda8654d3b82f
Syed Zaman Mostafiz <zamanwebdeveloper>
2019-09-17 00:46:51 +0600

    Update Some Working Things

--- themes/zamanwebdeveloper/template-fluid.php
+++ themes/zamanwebdeveloper/template-fluid.php
@@ -2,9 +2,10 @@
 /*
     Template Name: Full Width
 */
-get_header(); ?>
+get_header(); 
+?>
     <!-- Header area end -->
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+    <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
     <!-- News / Blog section  
     ============================================= -->
@@ -13,14 +14,14 @@ get_header(); ?>
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ftl">
                     <div class="single-page-details">
-                        <h2><?php the_title(); ?> This is template-fluid.php</h2>
+                        <h2><?php the_title(); ?> This is template-fluid</h2>
                         <?php the_content(); ?>                       
-                    </div>
+                    </div><p><?php the_tags(); ?></p>
                 </div>
             </div>
         </div>
     </div>
-    <!-- /news table  -->
+    <!-- news table  -->
     <?php endwhile; ?>
     <?php endif; ?>
 
